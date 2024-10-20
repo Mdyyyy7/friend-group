6b2ed265a21a9cd4f5aae26da9fadad4fac63bbc
comp0233 <ucapmox@ucl.ac.uk>
2024-10-20 21:10:38 +0800

    add personal information

--- group.py
+++ group.py
@@ -1,5 +1,61 @@
-"""An example of how to represent a group of acquaintances in Python."""
+jill = {
+    "name": "Jill",
+    "age": 26,
+    "job": "biologist",
+    "connections": [
+        {
+            "zalinka": "zalinka",
+            "connection": "friend"
+        },
+        {
+            "john": "john",
+            "connection": "partner"
 
-# Your code to go here...
+        }
+    ]
+}
 
-my_group =
+zalinka = {
+    "name": "Zalinka",
+    "age": 28,
+    "job": "artist",
+    "connections": [
+        {
+            "jill": "jill",
+            "connection": "friend"
+        },
+    ]
+}
+
+john = {
+    "name": "John",
+    "age": 27,
+    "job": "writer",
+    "connections": [
+        {
+            "jill": "jill",
+            "connection": "partner"
+        },
+    ]
+}
+
+nash = {
+    "name": "Nash",
+    "age": 34,
+    "job": "chef",
+    "connections": [
+        {
+            "john": "john",
+            "connection": "counsin"
+        },
+        {
+            "zalinka": "zalinka",
+            "connection": "landlord"
+        },
+    ]
+}
+
+my_group = [jill, zalinka, john, nash]
+
+
+
